dec3d8d121abe256bfa277f6b9e0ae46ca8d8259
Ewelina Rugala <rugala@devanddeliver.com>
2022-09-18 21:40:12 +0200

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Box, Container } from '@mui/system';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import { WelcomePage } from './pages/WelcomePage/WelcomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
-       <WelcomePage></WelcomePage>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Container>
+        <Box display='flex' textAlign="center" justifyContent='center' mt={6}>
+        <Routes>
+            <Route path='/' element={<WelcomePage />} />
+            <Route path='/question' element={<WelcomePage />} />
+        </Routes>
+        </Box>
+      </Container>
+    </BrowserRouter>
   );
 }
 
--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,38 +1,41 @@
 import React, { useState } from 'react';
 
 import './WelcomePage.css'
-import { Input } from '@mui/material';
+import { Box, Input, Typography } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { getTestData } from '../../getTestData';
 
 export const WelcomePage = () => {
     const [isLoading, setIsLoading] = useState(false);
 
-    const handleSearch = async () => {
+    const searchTest = async () => {
         setIsLoading(true);
-        const input = document.getElementById('testName') as HTMLInputElement | null; 
-    const inputValue = input?.value;
-    if (inputValue) {
-        const data = await getTestData(inputValue);
-        // pass data 
-        setIsLoading(false);
+        const input = document.getElementById('testName') as HTMLInputElement | null;
+        const inputValue = input?.value;
+        if (inputValue) {
+            const data = await getTestData(inputValue);
+            // pass data 
+            console.log(data);
+            setIsLoading(false);
+        }
+    } 
 
-    }
-
+    const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') searchTest()
     }
 
     return (
-        <div className='content'>
-            <header>Podaj nazwę testu</header>
-            <Input id='testName' disableUnderline={true} className='input' placeholder='np. Parasuperlotnia'></Input>
-            <div className='buttonWrapper'>
-                <LoadingButton loading={isLoading} loadingPosition='center' variant='outlined' onClick={handleSearch} >Szukaj</LoadingButton>
+        <Box mt={30} className='content'>
+            <Typography variant='h4' className='welcomeHeader' >Podaj nazwę testu</Typography>
+            <Input id='testName' disableUnderline={true} className='input' placeholder='np. ulc-pp-2022' onKeyPress={handleSearch}></Input>
+            <Box className='buttonWrapper'>
+                <LoadingButton loading={isLoading} loadingPosition='center' variant='outlined' onClick={searchTest} >Szukaj</LoadingButton>
                        
-            </div>
+            </Box>
             <div>
                 
             </div>
 
-        </div>
+        </Box>
     )
 }
